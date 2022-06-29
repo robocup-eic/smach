8a433242d9a4f1afb9f9e322a982ff6189ab2595
earthtennison <earth.tennison@gmail.com>
2022-06-30 02:06:29 +0700

    add environment descriptor to gpsr

---
+++ smach_task/gpsr_task/util/environment_descriptor.py
@@ -0,0 +1,32 @@
+import yaml
+from geometry_msgs.msg import Pose
+
+class EnvironmentDescriptor:
+    def __init__(self, yaml_path = "../config/fur_data.yaml"):
+        self.yaml_path = yaml_path
+        self.data_yaml = self.read_yaml()
+
+    def read_yaml(self):
+        with open(self.yaml_path, "r") as f:
+            try:
+                yaml_data = yaml.safe_load(f)
+                return yaml_data
+            except yaml.YAMLError as exc:
+                print(exc)
+                return None
+    def get_robot_pose(self, name):
+        for data in self.data_yaml:
+            if data["name"] == name:
+                robot_pose = Pose()
+                robot_pose.position.x = data["robot_pose"]["position"]["x"]
+                robot_pose.position.y = data["robot_pose"]["position"]["y"]
+                robot_pose.position.z = data["robot_pose"]["position"]["z"]
+                robot_pose.orientation.x = data["robot_pose"]["orientation"]["x"]
+                robot_pose.orientation.y = data["robot_pose"]["orientation"]["y"]
+                robot_pose.orientation.z = data["robot_pose"]["orientation"]["z"]
+                robot_pose.orientation.w = data["robot_pose"]["orientation"]["w"]
+                return robot_pose
+
+if __name__ == "__main__":
+    ed = EnvironmentDescriptor()
+    print(ed.get_robot_pose("table1"))
