117921ed15aa91dc1dc760ea884389fe9d836474
ChayaAsa <chayaasa@gmail.com>
2022-07-10 17:22:00 +0700

    update joint realsense -> realsense_pitch_joint

--- smach_task/receptionist_task/main_receptionist.py
+++ smach_task/receptionist_task/main_receptionist.py
@@ -375,7 +375,7 @@ class Navigation(smach.State):
                 person_id      = person[0]
                 min_distance = 10000000
                 person_pose = person[1]
-                person_pose = transform_pose(person_pose, "realsense", "map")
+                person_pose = transform_pose(person_pose, "realsense_pitch_joint", "map")
                 closest_chair = -1
 
                 chairs_distance = []
