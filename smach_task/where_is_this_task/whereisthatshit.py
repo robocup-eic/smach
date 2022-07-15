91290976765d90a22933fdb585ffed7020ff06fd
walkie-eic <robocup.cu@gmail.com>
2022-07-15 16:38:41 +0700

    update where is it

--- smach_task/where_is_this_task/whereisthatshit.py
+++ smach_task/where_is_this_task/whereisthatshit.py
@@ -126,11 +126,47 @@ class go_to_Navigation():
         self.move_base_client.send_goal(goal)
 
 
+class Start_signal(smach.State):
+
+    def __init__(self):
+        rospy.loginfo('Initiating Start_signal state')
+        smach.State.__init__(self,outcomes=['continue_standby'])
+        self.FRAME_COUNT_LIMIT = 5
+        self.close_distance = 1 # meter
+    
+    def execute(self,userdata):
+        rospy.loginfo('Executing Start_signal state')
+        # wait for the door to open
+        global rs
+        # Detect door opening
+        x_pixel, y_pixel = 1280/2, 720/2
+        frame_count = 0
+
+        while True:
+            rospy.sleep(0.5)
+            distance = rs.get_coordinate(x_pixel, y_pixel)[2]
+            rospy.loginfo(distance)
+            # filter lower distance
+            if distance < 0.4:
+                continue
+            # check if have available frame consecutively
+            if frame_count >= self.FRAME_COUNT_LIMIT:
+                speak("Door open")
+                break
+            if distance > self.close_distance:
+                frame_count += 1
+            else:
+                frame_count = 0
+        
+        return 'continue_standby'
+
+
+
 class Standby(smach.State):
 
-    def __init(self):
+    def __init__(self):
         rospy.loginfo('Initiating Standby state')
-        smach.State.__init__(self, outcomes = ['continue_DIRECTION'])
+        smach.State.__init__(self, outcomes = ['continue_direction'])
     
     def execute(self, userdata):
         global location, stt
@@ -159,7 +195,7 @@ class Standby(smach.State):
                 speak("Where do you want to go?")
                 stt.listen()
                 start_time = time.time()
-        return 'continue_DIRECTION'
+        return 'continue_direction'
 
 class Direction(smach.State):
 
@@ -328,21 +364,16 @@ if __name__ == '__main__':
     sm = smach.StateMachine(outcomes=['Succeeded','Aborted'])
 
     with sm:
-        # smach.StateMachine.add('Start_signal',Start_signal(),
-        #                         transitions={'continue_Navigate_information_point':'Standby'})
-        smach.StateMachine.add('Standby',Standby(),
-                                transitions={'continue_DIRECTION':'Direction'}) 
-
+        smach.StateMachine.add('Start_signal',Start_signal(),transitions={'continue_standby':'Standby'})
+        smach.StateMachine.add('Standby',Standby(),transitions={'continue_direction':'Direction'}) 
+        smach.StateMachine.add('Direction',Direction(), transitions={'continue_FOLLOW':'FOLLOW'})
         # Create sub smach state machine
-        sm_follow = smach.Concurrence(outcomes=['STOP', 'ARRIVED'], default_outcome = 'ARRIVED', outcome_map = {'STOP':{'Stop_command':'continue_stop','Follow_person':'continue_stop','Get_bounding_box':'continue_stop','Check_location':'continue_stop'}})      
+        sm_follow = smach.Concurrence(outcomes=['STOP', 'ARRIVED'], default_outcome = 'ARRIVED', outcome_map = {'STOP':{'Planning':'continue_stop','Check_location':'continue_stop'}})      
         with sm_follow:
-            # smach.Concurrence.add('Stop_command',Stop_command())
-            smach.Concurrence.add('Follow_person',Planning()) #navigate
-            # smach.Concurrence.add('Check_location',Check_location()) #announce location
-        smach.StateMachine.add('FOLLOW', sm_follow, transitions={'STOP':'Lost','ARRIVED':'Navigate_information_point'})
+            smach.Concurrence.add('Planning',Planning()) #navigate
+            smach.Concurrence.add('Check_location',Check_location()) #announce location
+        smach.StateMachine.add('FOLLOW', sm_follow, transitions={'STOP':'Standby','ARRIVED':'Standby'})
         #------------------------------------
-        smach.StateMachine.add('Lost',Lost(),
-                                transitions={'continue_FOLLOW':'FOLLOW','continue_Navigate_information_point':'Navigate_information_point'})
         # Set up                                                    
         sis = smach_ros.IntrospectionServer('Server_name',sm,'/PatterRoot')
         sis.start()
