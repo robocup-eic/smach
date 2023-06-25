a4952692ff98b0e73a9497c41e4fd471fbbfceae
suppakitjom <99940355+suppakitjom@users.noreply.github.com>
2023-06-25 08:42:24 +0700

    restaurant task

--- smach_task_2023/task_restaurant.py
+++ smach_task_2023/task_restaurant.py
@@ -6,19 +6,28 @@ import smach_ros
 import nlp_client
 from ratfin import *
 from person import Person
-from utils import (WakeWord, Speak, GetIntent, GetName, GetObject, GetLocation,GetEntities)
+from core_nlp.utils import (WakeWord, Speak, GetIntent, GetName, GetObject,
+                            GetLocation, GetEntities)
+from core_cv.pose_estimation import PoseEstimation
+from core_smach.move_to import MoveTo
+from core_smach.grasp_object import GraspObject
+from core_smach.place_object import PlaceObject
+
 
 class RestaurantDecision(smach.State):
     def __init__(self):
         smach.State.__init__(self,
-                             outcomes=['checkout','order','cancel'],input_keys=['intent']
-                             )
+                             outcomes=['out1', 'out2'],
+                             input_keys=['intent', 'object'])
+
     def execute(self, userdata):
-        if userdata.intent == 'restaurant_checkout':
-            return 'checkout'
-        elif userdata.intent == 'restaurant_order':
-            return 'order'
-        return 'cancel'
+        if userdata.intent == 'deny':
+            return 'out0'
+        elif userdata.intent == 'restaurant_order' and userdata.object:
+            return 'out1'
+        else:
+            return 'out2'
+
 
 class DummyIdle(smach.State):
     '''
@@ -26,42 +35,103 @@ class DummyIdle(smach.State):
     CANT BREATH WITH ALL THAT ASTHHH MAAAA
     '''
     def __init__(self):
-        smach.State.__init__(self,
-                             outcomes=['out1']
-                             )
+        smach.State.__init__(self, outcomes=['out1'])
 
     def execute(self, userdata):
         return 'out1'
+
 
 def main():
     rospy.init_node('smach_example_state_machine')
 
     # Create a SMACH state machine
-    sm = smach.StateMachine(outcomes=['out1','out0'])
+    sm = smach.StateMachine(outcomes=['out1', 'out0'])
 
     # Open the container
     with sm:
-        # Add states to the container
-        smach.StateMachine.add('IDLE', DummyIdle(), 
-                               transitions={'out1':'SPEAK_ASK'})
-        
-        smach.StateMachine.add('SPEAK_ASK', Speak(text="Hello bitch! How can I help you today?"), 
-                               transitions={'out1':'LISTEN_INTENT'})
-        
-        smach.StateMachine.add('LISTEN_INTENT',GetEntities(intent=True,object=True,timeout=5),
-                               transitions={'out1':'TASK_TRANSITION'},
-                               remapping={'listen_intent':'intent'})
-        
-        smach.StateMachine.add('TASK_TRANSITION',RestaurantDecision(),
-                               transitions={'cancel':'IDLE', 'checkout':'SPEAK_SUMMARIZE_ORDER', 'order':'PROCESS_ORDER'},
-                               remapping={'intent':'intent'})
-        
-        smach.StateMachine.add('SPEAK_SUMMARIZE_ORDER',Speak(text='you ordered something!'),
-                               transitions={'out1':'out0'})
-        
-        smach.StateMachine.add('PROCESS_ORDER',Speak(text='order something!'),
-                               transitions={'out1':'out0'})
+        # TODO Pose-Estimation for raising hand
+        smach.StateMachine.add('IDLE',
+                               DummyIdle(),
+                               transitions={'out1': 'MOVE_TO_CUSTOMER'})
 
+        smach.StateMachine.add('MOVE_TO_CUSTOMER',
+                               MoveTo(),
+                               transitions={'out1': 'ASK_CUSTOMER'})
+
+        smach.StateMachine.add(
+            'ASK_CUSTOMER',
+            Speak(text='Hello! Can I get you something?'),
+            transitions={'out1': 'COMPUTE_COMMAND'},
+        )
+
+        smach.StateMachine.add(
+            'COMPUTE_COMMAND',
+            GetEntities(intent=True, object=True),
+            transitions={
+                'out1': 'RESTAURANT_DECISION',
+                'out2': 'SPEAK_REORDER'
+            },
+            remapping={
+                'intent': 'intent',
+                'object': 'object'
+            },
+        )
+
+        smach.StateMachine.add(
+            'RESTAURANT_DECISION',
+            RestaurantDecision(),
+            transitions={
+                'out1': 'MOVE_TO_KITCHEN',
+                'out0': 'SPEAK_REORDER',
+                'out2': 'SPEAK_DONE'
+            },
+            remapping={'intent': 'intent'},
+        )
+
+        smach.StateMachine.add(
+            'SPEAK_DONE',
+            Speak(text='Okay, you can call me anytime if you need anything!'),
+            transitions={'out1': 'IDLE'},
+        )
+
+        smach.StateMachine.add(
+            'SPEAK_REORDER',
+            Speak(
+                text=
+                'Sorry, I did not get that. Can you repeat? Please note that I can only take one order at a time.'
+            ),
+            transitions={'out1': 'COMPUTE_COMMAND'},
+        )
+
+        smach.StateMachine.add(
+            'MOVE_TO_KITCHEN',
+            MoveTo(),
+            transitions={'out1': 'GRASP_OBJECT'},
+        )
+
+        smach.StateMachine.add(
+            'GRASP_OBJECT',
+            GraspObject(),
+            transitions={'out1': 'PLACE_OBJECT'},
+        )
+
+        smach.StateMachine.add(
+            'PLACE_OBJECT',
+            PlaceObject(),
+            transitions={'out1': 'SPEAK_HERE_IS_YOUR_ORDER'},
+        )
+
+        smach.StateMachine.add(
+            'SPEAK_HERE_IS_YOUR_ORDER',
+            Speak(text='Here is your order!'),
+            transitions={'out1': 'SPEAK_ANYTHING_ELSE'},
+        )
+
+        smach.StateMachine.add(
+            'SPEAK_ANYTHING_ELSE',
+            Speak(text='Is there anything else I can get you?'),
+            transitions={'out1': 'COMPUTE_COMMAND'},
+        )
 
     # Execute SMACH plan
     outcome = sm.execute()
