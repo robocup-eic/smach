8e6cbc24f5ad5f37b931ad8197f43597547ba5e8
suppakitjom <99940355+suppakitjom@users.noreply.github.com>
2023-06-30 19:59:47 +0200

    added move_base

--- smach_task_2023/core_smach/move_to.py
+++ smach_task_2023/core_smach/move_to.py
@@ -9,38 +9,43 @@ import smach_ros
 import nlp_client
 import threading
 from ratfin import *
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import actionlib
+from actionlib_msgs.msg import *
+from tf.transformations import quaternion_from_euler
+from geometry_msgs.msg import Pose, Point, Quaternion
 
 def print_available_userdata(userdata):
     print(userdata)
 
 # MoveTo state
 class MoveTo(smach.State):
-    """ 
-    TemplateVersion 1.1.0 
-    MoveTo state
+"""
+TemplateVersion 1.1.0
+MoveTo state
 
-    BASIC FUNCTION:
-    Move from A to B with CV to find object/person
-    1. [NLP] room, object, furniture, person, furniture_adjective,
-        - [room, object, furniture, person] one must not be null
-    2. [NAVIGATION] Move to the specified location
-    3. [COMPUTER VISION] Detect the object/person. If not detected, repeat step 2
-    3.1 [NLP] Announce "Unable to detect the object/person. Please try again" --> Loop
-    4. [NLP] Announce "I have reached the location" --> out1
+BASIC FUNCTION:
+Move from A to B with CV to find object/person
+1. [NLP] room, object, furniture, person, furniture_adjective,
+    - [room, object, furniture, person] one must not be null
+2. [NAVIGATION] Move to the specified location
+3. [COMPUTER VISION] Detect the object/person. If not detected, repeat step 2
+3.1 [NLP] Announce "Unable to detect the object/person. Please try again" --> Loop
+4. [NLP] Announce "I have reached the location" --> out1
 
-    """
+"""
 
-    def __init__(self, 
+    def __init__(self,
                  log : bool = False,
                  timeout_tries: int = 0 # 0 means infinite tries
                  ):
-        
+
         # Raise exceptions if any entity parameter is not of type bool
         if not isinstance(log, bool):
             raise ValueError("Argument 'log' must be of type bool")
-        
+
         # Initialize the state
-        smach.State.__init__(self, 
+        smach.State.__init__(self,
                                 outcomes=['out1'],
                             #  outcomes=['out1','out2','loop','undo','timeout'],
                              input_keys=['room','furniture','data3'],
@@ -56,7 +61,6 @@ class MoveTo(smach.State):
 
         # Set self.variables
 
-
     def execute(self, userdata):
         try:
             # Log the execution stage
@@ -65,7 +69,6 @@ class MoveTo(smach.State):
             # Userdata verification
             rospy.loginfo(f'(MoveTo): Checking userdata..')
 
-
             # Check if the userdata room, object,furniture is None
             if (userdata.room == "") and (userdata.furniture == "") and (userdata.object == "") and (userdata.person == ""):
                 raise Exception(
@@ -73,9 +76,9 @@ class MoveTo(smach.State):
             else:
                 # if room is provided
 
-                if (userdata.room != ""): 
+                if (userdata.room != ""):
                     # if furniture is provided
-                    if userdata.furniture != "": 
+                    if userdata.furniture != "":
                         # if furniture is found, move to the furniture (add nlp trying to find the furniture)
                         if userdata.person != "": # room+furniture+person
                             #TODO turn on CV to try to detect the person around the furniture in the room
@@ -98,11 +101,11 @@ class MoveTo(smach.State):
                         return "out1"
 
                 # if room is not provided and furniture is provided
-                elif (userdata.furniture != ""): 
-                    if userdata.object != "": # furniture+object 
+                elif (userdata.furniture != ""):
+                    if userdata.object != "": # furniture+object
                         #TODO turn on CV to try to detect the object around the furniture (360 around furniture)
                         return "out1"
-                    elif (userdata.person != ""): # furniture+person 
+                    elif (userdata.person != ""): # furniture+person
                         #TODO turn on CV to try to detect the person at the furniture (360 around furniture)
                         return "out1"
                     else: # furniture
@@ -110,30 +113,98 @@ class MoveTo(smach.State):
                         return "out1"
 
                 # find the first person around the arena
-                elif (userdata.person != ""):  # person 
+                elif (userdata.person != ""):  # person
                     #TODO turn on CV to try to detect the person in the entire arena
                     return "out1"
                 else:
                     raise Exception("How the fuck did you get here!")
 
-
-
-
-
-
             # while (self.tries_counter < self.timeout_tries) or not self.timeout_bool:
             #     # Increment the counter
             #     self.tries_counter += 1
-
 
             # if something goes wrong, raise exception
             if False:
                 raise Exception(
                     "(MoveTo): No attribute detected in the timeout period")
-            
+
             return "out1"
         except Exception as e:
             printclr(e, "red")
             return "undo"
 
 
+class MoveTo_move_base(smach.State):
+    """
+    TemplateVersion 1.1.0
+    MoveTo state
+
+    BASIC FUNCTION:
+    Move from A to B with CV to find object/person
+    1. [NLP] room, object, furniture, person, furniture_adjective,
+        - [room, object, furniture, person] one must not be null
+    2. [NAVIGATION] Move to the specified location
+    3. [COMPUTER VISION] Detect the object/person. If not detected, repeat step 2
+    3.1 [NLP] Announce "Unable to detect the object/person. Please try again" --> Loop
+    4. [NLP] Announce "I have reached the location" --> out1
+
+    """
+    def __init__(self,
+                 log : bool = False,
+                 timeout_tries: int = 0, # 0 means infinite tries
+                 target_pose: Pose = None
+                 ):
+        # Raise exceptions if any entity parameter is not of type bool
+        if not isinstance(log, bool):
+            raise ValueError("Argument 'log' must be of type bool")
+        if target_pose is None:
+            raise ValueError("Argument 'target_pose' must not be None")
+
+        rospy.init_node('nav_test', anonymous=False)
+        rospy.on_shutdown(self.shutdown)
+        #tell the action client that we want to spin a thread by default
+        self.move_base = actionlib.SimpleActionClient("move_base",
+                                                      MoveBaseAction)
+        rospy.loginfo("wait for the action server to come up")
+        #allow up to 5 seconds for the action server to come up
+        self.move_base.wait_for_server(rospy.Duration(5))
+        # Initialize the state
+        smach.State.__init__(self,
+                                outcomes=['out1'],
+                            #  outcomes=['out1','out2','loop','undo','timeout'],
+                             input_keys=['room','furniture','data3'],
+                             output_keys=['data1','data3'])
+
+        # timout configuration, (don't change)
+        if timeout_tries == 0:
+            self.timeout_bool = False
+        else:
+            self.timeout_bool = True
+        self.tries_counter = int(0) # counter for the number of tries
+        self.timeout_tries = timeout_tries
+        self.pose = target_pose
+        
+
+    def execute(self,userdata):
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = 'map'
+        goal.target_pose.header.stamp = rospy.get_rostime()
+        goal.target_pose.pose = self.pose
+        self.move_base.send_goal(goal)
+        print("Waiting for result")
+        success = self.move_base.wait_for_result(rospy.Duration(60))
+        print("Return result: ", success)
+        state = self.move_base.get_state()
+        result = False
+        if success and state == GoalStatus.SUCCEEDED:
+            # We made it!
+            result = True
+        else:
+            self.move_base.cancel_goal()
+            self.goal_sent = False
+        return result
+
+    def shutdown(self):
+        stop_goal = MoveBaseGoal()
+        self.move_base.send_goal(stop_goal)
+        rospy.loginfo("Stop")
